af3193d5e57db6cc6d5a0cd78b5b3354db5e25b9
102306071 <102306071@nccu.edu.tw>
2015-03-21 22:14:28 +0800

    修正bug 等於後打數字問題
    修正等於後打數字會直接加上去
    1+2=3  按0  >> 30
    
    修正1+0 = NaN問題

--- app.js
+++ app.js
@@ -6,45 +6,121 @@ $(function(){
 
 
 	$('#btn0').on('click',function(){
-		if(currentText!=''){
+		 if(operator==''){
+			// alert('hi');
+			currentText='0';
+			operator='';
+			render();
+			update();
+			$('#operator').html("&nbsp");
+		}else if(currentText!=''){
+			// alert('fuck!!');
+			currentText =currentText+'0';
+			render();
+		}else if(preText!=''){
+			// alert('hello')
 			currentText =currentText+'0';
 			render();
 		}
 	 })
 
 	$('#btn1').on('click',function(){
+		if(preText==''){
+			currentText='1';
+			operator='';
+			render();
+			update();
+			$('#operator').html("&nbsp");
+		}
+
 		currentText =currentText+'1';
 		render();
 	 })
 	$('#btn2').on('click',function(){
+		if(preText==''){
+			currentText='2';
+			operator='';
+			render();
+			update();
+			$('#operator').html("&nbsp");
+		}
 		currentText =currentText+'2';
 		render();
 	 })
 	$('#btn3').on('click',function(){
+		if(preText==''){
+			currentText='3';
+			operator='';
+			render();
+			update();
+			$('#operator').html("&nbsp");
+		}
 		currentText =currentText+'3';
 		render();
 	 })
 	$('#btn4').on('click',function(){
+		if(preText==''){
+			currentText='4';
+			operator='';
+			render();
+			update();
+			$('#operator').html("&nbsp");
+		}
 		currentText =currentText+'4';
 		render();
 	 })
 	$('#btn5').on('click',function(){
+		if(preText==''){
+			currentText='5';
+			operator='';
+			render();
+			update();
+			$('#operator').html("&nbsp");
+		}
 		currentText =currentText+'5';
 		render();
 	 })
 	$('#btn6').on('click',function(){
+		if(preText==''){
+			currentText='6';
+			operator='';
+			render();
+			update();
+			$('#operator').html("&nbsp");
+		}
 		currentText =currentText+'6';
 		render();
 	 })
 	$('#btn7').on('click',function(){
+		if(preText==''){
+			currentText='7';
+			operator='';
+			render();
+			update();
+			$('#operator').html("&nbsp");
+		}
 		currentText =currentText+'7';
 		render();
 	 })
 	$('#btn8').on('click',function(){
+		if(preText==''){
+			currentText='8';
+			operator='';
+			render();
+			update();
+			$('#operator').html("&nbsp");
+		}
 		currentText =currentText+'8';
 		render();
 	 })
 	$('#btn9').on('click',function(){
+		if(preText==''){
+			currentText='9';
+			operator='';
+			render();
+			update();
+			$('#operator').html("&nbsp");
+		}
 		currentText =currentText+'9';
 		render();
 	 })
@@ -117,7 +193,7 @@ $(function(){
 	$('#btnEquel').on('click',function(){
 		preCalculate();
 		$('#operator').html('&nbsp');
-		 operator='';
+		operator='';
 		 // alert('operator = '+operator);
 		 // alert(currentText);
 	 })
